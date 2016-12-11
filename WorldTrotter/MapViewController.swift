ce9694fa08b49eb4cd5edcd834b7b3a856512003
Qiao Zhang <qiao.zhang.2015@gmail.com>
2016-12-11 09:15:18 -0500

    programmatically add MapView

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,7 +7,62 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-  
+  var mapView: MKMapView!
+}
+
+// MARK: - View Life Cycle
+extension MapViewController {
+  override func loadView() {
+    mapView = MKMapView()
+    view = mapView
+    
+    let segmentedControl =
+      UISegmentedControl(items: ["Standard", "Hybird", "Satellite"])
+    segmentedControl.backgroundColor =
+      UIColor.white.withAlphaComponent(0.5)
+    segmentedControl.selectedSegmentIndex = 0
+    
+    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(segmentedControl)
+    
+    let marginsGuide = view.layoutMarginsGuide
+    
+    let topConstraint =
+      segmentedControl.topAnchor.constraint(
+        equalTo: topLayoutGuide.bottomAnchor)
+    
+    let leadingConstraint =
+      segmentedControl.leadingAnchor.constraint(
+        equalTo: marginsGuide.leadingAnchor)
+    let trailingConstraint =
+      segmentedControl.trailingAnchor.constraint(
+        equalTo: marginsGuide.trailingAnchor)
+    
+    [topConstraint, leadingConstraint, trailingConstraint].forEach {
+      $0.isActive = true
+    }
+    
+    segmentedControl.addTarget(
+      self,
+      action: #selector(MapViewController.mapTypeChanged),
+      for: .valueChanged)
+  }
+}
+
+private extension MapViewController {
+  @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+    switch segControl.selectedSegmentIndex {
+    case 0:
+      mapView.mapType = .standard
+    case 1:
+      mapView.mapType = .hybrid
+    case 2:
+      mapView.mapType = .satellite
+    default:
+      break
+    }
+  }
 }
